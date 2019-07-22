b8f627287fd985f162ee16dc429ba91aa1d2060d
vvele017 <vvele017@fiu.edu>
2019-07-22 02:33:23 -0400

    Testing change

--- src/graphql/types/Review.ts
+++ src/graphql/types/Review.ts
@@ -5,8 +5,9 @@ const Reviews = objectType({
   description: 'User comment and star rating (review) for a book',
   definition(t) {
     t.id('id');
-    t.id('user_id');
-    t.id('book_id');
+    t.field('user_id', {
+      type: 'User'
+    });
     t.string('title');
     t.string('body');
     t.int('rating');
