f3c77b71c6a86bb8817ba575fdc610926f1f261b
Matt Taylor <matttmiami15@gmail.com>
2019-07-22 23:46:38 -0400

    rework query to use Context authentication

--- src/graphql/mutations/AddToShoppingCart.ts
+++ src/graphql/mutations/AddToShoppingCart.ts
@@ -1,12 +1,12 @@
+import { AuthenticationError } from 'apollo-server-core';
 import { mutationField, arg, inputObjectType } from 'nexus';
 
+import { Context, isAuthenticated } from '../context/Context';
 import { addToShoppingCart, getShoppingCartByUserId } from '../../data/shoppingCart';
 
 const AddToShoppingCartInput = inputObjectType({
   name: 'AddToShoppingCartInput',
   definition(t) {
-    t.string('user_id', {required: true});
-    // TODO: Replace this user_id line with code to get id of logged-in user
     t.string('book_id', {required: true});
     t.int('amount', {required: true});
   }
@@ -20,8 +20,11 @@ const AddToShoppingCart = mutationField('add_to_shopping_cart', {
       required: true
     })
   },
-  async resolve(root, { input: { user_id, book_id, amount } }) {
-    const cart = await getShoppingCartByUserId(user_id);
+  async resolve(root, { input: { book_id, amount }}, ctx: Context ) {
+    if (!isAuthenticated(ctx)) {
+      throw new AuthenticationError('Unauthenticated');
+    }
+    const cart = await getShoppingCartByUserId(ctx.viewer.id);
     const { id } = await addToShoppingCart(cart.id, book_id, amount);
     return { id, amount };
   }
